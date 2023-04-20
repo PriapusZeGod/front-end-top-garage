89a54de45abb0feeabdf21e8b349462e668ebfbf
Iurie Gavriliuc <iurg2002@gmail.com>
2023-04-20 15:40:58 +0200

    routing, garage drop-down, garage widget

---
+++ src/components/Garage.js
@@ -0,0 +1,100 @@
+import { useState } from "react";
+import Dropdown from "react-bootstrap/Dropdown";
+import gclass from "../images/g-class.png"
+
+export default function GarageList() {
+  let garages = [
+    {
+      id: 1,
+      name: "garage1",
+    },
+    {
+      id: 2,
+      name: "garage2",
+    },
+    {
+      id: 3,
+      name: "garage3",
+    },
+  ];
+
+  const [garage, setGarage] = useState(garages[0]);
+
+  return (
+    <>
+      <div className="container">
+        <div className="row">
+          <h1>{garage.name}</h1>
+          <GarageDropdown
+            garages={garages}
+            currentGarage={garage}
+            setCurrentGarage={setGarage}
+          />
+        </div>
+
+        <div className="row mt-5">
+          <div className="col-sm-4 mt-3">
+            <GarageWidget garage={garages[0]} />
+          </div>
+          <div className="col-sm-4 mt-3">
+            <GarageWidget garage={garages[1]} />
+          </div>
+          <div className="col-sm-4 mt-3">
+            <GarageWidget garage={garages[2]} />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+function GarageDropdown({ garages, currentGarage, setCurrentGarage }) {
+  return (
+    <Dropdown>
+      <Dropdown.Toggle variant="dark" id="dropdown-basic">
+        {currentGarage.name}
+      </Dropdown.Toggle>
+
+      <Dropdown.Menu>
+        {garages.map((g) => (
+          <Dropdown.Item key={g.id} onClick={() => setCurrentGarage(g)}>
+            {g.name}
+          </Dropdown.Item>
+        ))}
+      </Dropdown.Menu>
+    </Dropdown>
+  );
+}
+
+function GarageWidget({garage}) {
+  return (
+    <>
+      <div className="bg-primary rounded">
+        <div className="row">
+          <h1 className="text-center text-light mt-2">{garage.name}</h1>
+          <div className="col m-2">
+            <div className="row">
+              <div className="text-center text-light fw-bold mt-2">
+                Free Spots:
+              </div>
+            </div>
+            <div className="row">
+              <div className="text-center text-light fw-bolder fs-2">10/10</div>
+            </div>
+          </div>
+          <div className="col text-start">
+            <ul className="text-light m-2">
+              <li>Lamborghini</li>
+              <li>Toyota</li>
+              <li>BMW</li>
+            </ul>
+          </div>
+        </div>
+
+        <div className="row">
+          <img src={gclass} />
+        </div>
+      </div>
+    </>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,15 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import GarageList from './components/Garage';
 import reportWebVitals from './reportWebVitals';
+import { RouterProvider, createHashRouter } from "react-router-dom"
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+const router = createHashRouter([
+  {
+      path: "/",
+      element: <h1>Home</h1>,
+  },
+  {
+      path: "/about",
+      element: <h1>About</h1>,
+  },
+  {
+    path: "/profile",
+    element: <GarageList/>,
+},
+])
+
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<RouterProvider router={router} />)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
