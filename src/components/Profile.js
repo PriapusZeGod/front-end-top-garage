8afca246b34e872ce1a6ab2fce9c99b013ba990a
Iurie Gavriliuc <iurg2002@gmail.com>
2023-05-10 17:17:38 +0200

    Connect Profile to API & Possibility to edit personal information

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -2,39 +2,35 @@ import React, { useEffect } from "react";
 import profileImg from "../images/profile.webp";
 import { useState } from "react";
 import GarageList from "./Garage";
+import { getProfileById } from "../services/profileService";
+import ProfileEditModal from "./ProfileEditModal";
 
 const url = "http://localhost:5158";
 
-export default function Profile() {
-  const [profiles, setProfiles] = useState([{
-    id: 0,
-    name: "Unknown",
-    email: "unknown",
-    age: "0",
-    phone: "0",
-  }]);
+export default function Profile({ userId }) {
   const [profile, setProfile] = useState({
     id: 0,
     name: "Unknown",
-    email: "unknown",
+    email: "unknown@mail",
     age: "0",
     phone: "0",
   });
+  let id = userId;
+  if (userId == null) {
+    id = 1;
+  }
 
   useEffect(() => {
     console.log("works");
     fetchProfiles();
   }, []);
 
-  useEffect(() => {
-    setProfile(profiles[0]);
-  }, [profiles]);
-
   const fetchProfiles = async () => {
-    const response = await getProfiles();
-    const data = await response.json();
-    setProfiles(await data);
-    console.log(data);
+    setProfile(await getProfileById(1));
+  };
+
+  const updateObjectState = (updatedObject) => {
+    setProfile(updatedObject);
   };
 
   return (
@@ -52,62 +48,27 @@ export default function Profile() {
           </div>
           <div className="col-md-6">
             <p className="fs-1 ">
-              Name: <span className="fw-bolder">{profiles[0].name}</span>
+              Name: <span className="fw-bolder">{profile.name}</span>
+            </p>
+            <p className="fs-1 ">
+              Age: <span className="fw-bolder">{profile.age}</span>
             </p>
             <p className="fs-1">
-              Surname: <span className="fw-bolder">{profiles[0].name}</span>
+              E-mail: <span className="fw-bolder">{profile.email}</span>
             </p>
             <p className="fs-1">
-              E-mail: <span className="fw-bolder">{profiles[0].email}</span>
-            </p>
-            <p className="fs-1">
-              Phone Number: <span className="fw-bolder">{profiles[0].phone}</span>
+              Phone Number: <span className="fw-bolder">{profile.phone}</span>
             </p>
           </div>
         </div>
+        <div className="row">
+          <div className="col-md-12 text-center">
+            <ProfileEditModal profile={profile} setProfile={updateObjectState} />
+          </div>
+        </div>
       </div>
-      <GarageList />
 
+      <GarageList />
     </>
   );
 }
-
-async function getProfiles() {
-  const headers = {
-    "Content-Type": "application/json",
-    "Access-Control-Allow-Origin": "*",
-    Accept: "*/*",
-    Host: "localhost:5158",
-    // Add any additional headers you want to include
-  };
-
-  const response = await fetch(`${url}/Users`, {
-    headers: headers,
-    mode: "no-cors",
-  });
-
-  console.log(await response);
-
-
-  return response;
-}
-
-
-// export default function App() {
-//   const [responseText, setResponseText] = useState('');
-
-//   useEffect(() => {
-//     async function fetchData() {
-//       const response = await fetch('https://jsonplaceholder.typicode.com/todos/1');
-//       const data = await response.text();
-//       setResponseText(data);
-//     }
-//     fetchData();
-//   }, []);
-
-//   return (
-//     <div>
-//       <p>{responseText}</p>
-//     </div>
-//   );
-// }
--- src/services/profileService.js
+++ src/services/profileService.js
@@ -1,4 +1,34 @@
-async function getProfileById(id)
-{
-    
+const url = "http://localhost:5158/Users";
+
+export async function getProfileById(id) {
+  const response = await fetch(url);
+  const data = await response.json();
+  const user = await data.find((user) => user.id === id);
+  return user;
+}
+
+export async function updateProfile(user) {
+  console.log("User: " + JSON.stringify(user));
+  try {
+    const response = await fetch(url, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(user),
+    });
+    if (response.status != 204) {
+      try {
+        const data = await response.json();
+        console.log("Success:", data);
+        return data;
+      } catch (error) {
+        console.error("Error:", error);
+      }
+    }
+  } catch (error) {
+    console.error("Error:", error);
+  }
+  console.log("Success");
+  return null;
 }
