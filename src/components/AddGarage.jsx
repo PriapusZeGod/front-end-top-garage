d40aff05fb24a1a7f4934399b6260757c0978fec
kotruta123 <315175@viauc.dk>
2023-05-25 22:08:30 +0200

    Some validation

--- src/components/AddGarage.jsx
+++ src/components/AddGarage.jsx
@@ -1,7 +1,10 @@
 import React, { useState, useContext } from 'react';
 import { createGarages } from "../services/GarageService";
 import { Box, Button, FormControl, FormErrorMessage, FormLabel, Input, Select, VStack } from "@chakra-ui/react";
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import UserContext from "./UserContext";
+import MapViewComponent from "./MapView";
 
 const STORAGE_KEY = "userAuth";
 
@@ -14,7 +17,7 @@ const AddGarage = () => {
             latitude: '',
             longitude: ''
         },
-        numberOfCars: 0,
+        numberOfCars: '0',
     });
     const [formErrors, setFormErrors] = useState({});
 
@@ -54,6 +57,9 @@ const AddGarage = () => {
         if (garage.location.longitude.trim() === '') {
             errors.longitude = 'Longitude is required';
         }
+        if (garage.numberOfCars === '0') {
+            errors.numberOfCars = 'Number of Cars is required';
+        }
 
         if (Object.keys(errors).length > 0) {
             setFormErrors(errors);
@@ -69,17 +75,26 @@ const AddGarage = () => {
 
             const response = await createGarages(user, id, name, capacity, location);
             console.log('Garage added successfully:', response);
+            toast.success('Garage added successfully', {
+                position: toast.POSITION.BOTTOM_RIGHT
+            });
+            setTimeout(() => {
+                window.location.reload();
+            }, 1000);
         } catch (error) {
             console.error('Error adding garage:', error);
+            toast.error('Error adding garage', {
+                position: toast.POSITION.BOTTOM_RIGHT
+            });
         }
     };
 
     return (
         <Box>
-            <Box as="h2" mb={4} fontSize="xl" fontWeight="bold">
-                Add Garage
-            </Box>
             <form onSubmit={handleSubmit} style={{ maxWidth: '400px', margin: '0 auto' }}>
+                <Box as="h2" mb={4} fontSize="xl" fontWeight="bold">
+                    Add Garage
+                </Box>
                 <VStack spacing={4}>
                     <FormControl isInvalid={!!formErrors.name}>
                         <FormLabel htmlFor="name">Garage Name:</FormLabel>
@@ -89,38 +104,46 @@ const AddGarage = () => {
                             name="name"
                             value={garage.name}
                             onChange={handleInputChange}
+                            _focus={{ outline: 'none' }}
+                            required
                         />
                         <FormErrorMessage>{formErrors.name}</FormErrorMessage>
                     </FormControl>
                     <FormControl isInvalid={!!formErrors.latitude}>
                         <FormLabel htmlFor="latitude">Latitude:</FormLabel>
                         <Input
-                            type="text"
+                            type="number"
                             id="latitude"
                             name="latitude"
                             value={garage.location.latitude}
                             onChange={handleInputChange}
+                            _focus={{ outline: 'none' }}
+                            required
                         />
                         <FormErrorMessage>{formErrors.latitude}</FormErrorMessage>
                     </FormControl>
                     <FormControl isInvalid={!!formErrors.longitude}>
                         <FormLabel htmlFor="longitude">Longitude:</FormLabel>
                         <Input
-                            type="text"
+                            type="number"
                             id="longitude"
                             name="longitude"
                             value={garage.location.longitude}
                             onChange={handleInputChange}
+                            _focus={{ outline: 'none' }}
+                            required
                         />
                         <FormErrorMessage>{formErrors.longitude}</FormErrorMessage>
                     </FormControl>
-                    <FormControl>
+                    <FormControl isInvalid={!!formErrors.numberOfCars}>
                         <FormLabel htmlFor="numberOfCars">Number of Cars:</FormLabel>
                         <Select
                             id="numberOfCars"
                             name="numberOfCars"
                             value={garage.numberOfCars}
                             onChange={handleInputChange}
+                            _focus={{ outline: 'none' }}
+                            required
                         >
                             <option value="0">0</option>
                             <option value="1">1</option>
@@ -130,13 +153,21 @@ const AddGarage = () => {
                             <option value="5">5</option>
                             <option value="6">6</option>
                         </Select>
+                        <FormErrorMessage>{formErrors.numberOfCars}</FormErrorMessage>
                     </FormControl>
-                    <Box>{/* Add map component here for choosing location */}</Box>
-                    <Button type="submit" colorScheme="blue">
+                    <Button
+                        type="submit"
+                        colorScheme="blue"
+                        _hover={{ transform: 'scale(1.05)', transition: 'transform 0.2s' }}
+                        _focus={{ outline: 'none' }}
+                    >
                         Add Garage
                     </Button>
                 </VStack>
+                <br/>
+                <MapViewComponent />
             </form>
+            <ToastContainer />
         </Box>
     );
 };
--- src/components/Addcar.jsx
+++ src/components/Addcar.jsx
@@ -8,6 +8,7 @@ import {
     Input,
     Select,
     Textarea,
+    useToast,
 } from "@chakra-ui/react";
 import UserContext from "./UserContext";
 import { getGaragesByUserID } from "../services/GarageService";
@@ -17,21 +18,32 @@ const STORAGE_KEY = "userAuth";
 
 export function GarageDropdown({ garages, handleGarageSelect }) {
     return (
-        <FormControl>
+        <FormControl isInvalid={!garages || garages.length === 0}>
             <FormLabel htmlFor="garage">Garage:</FormLabel>
-            <Select id="garage" name="garage" onChange={handleGarageSelect}>
-                <option value="">Select a Garage</option>
+            <Select
+                id="garage"
+                name="garage"
+                onChange={handleGarageSelect}
+                defaultValue=""
+            >
+                <option value="" disabled hidden>
+                    Select a Garage
+                </option>
                 {garages.map((garage) => (
                     <option key={garage.id} value={garage.id}>
                         {garage.name}
                     </option>
                 ))}
             </Select>
+            {!garages || garages.length === 0 ? (
+                <FormErrorMessage>No garages available</FormErrorMessage>
+            ) : null}
         </FormControl>
     );
 }
 
 const AddCar = () => {
+    const toast = useToast();
     const { user } = useContext(UserContext);
     const [car, setCar] = useState({
         name: "",
@@ -52,7 +64,7 @@ const AddCar = () => {
     const [garages, setGarages] = useState([]);
 
     useEffect(() => {
-        const userID = user.id; // Assuming user object has an 'id' property
+        const userID = user.id;
         if (userID) {
             getGaragesByUserID(userID)
                 .then((response) => {
@@ -116,9 +128,48 @@ const AddCar = () => {
         }
         if (car.year.trim() === "") {
             errors.year = "Year is required";
+        } else if (
+            parseInt(car.year.trim()) < 1800 ||
+            parseInt(car.year.trim()) > 2200
+        ) {
+            errors.year = "Year must be between 1800 and 2200";
         }
         if (car.seats.trim() === "") {
             errors.seats = "Seats is required";
+        } else if (
+            parseInt(car.seats.trim()) < 1 ||
+            parseInt(car.seats.trim()) > 100
+        ) {
+            errors.seats = "Number of seats must be between 1 and 100";
+        }
+        if (car.engine.size.trim() === "") {
+            errors.engineSize = "Engine Size is required";
+        } else if (
+            parseFloat(car.engine.size.trim()) < 0.1 ||
+            parseFloat(car.engine.size.trim()) > 10
+        ) {
+            errors.engineSize = "The engine size must be between 0.1 and 10";
+        }
+        if (car.engine.fuelType.trim() === "") {
+            errors.engineFuelType = "Engine Fuel Type is required";
+        }
+        if (car.engine.powerHP.trim() === "") {
+            errors.enginePowerHP = "Engine Power (HP) is required";
+        } else if (
+            parseInt(car.engine.powerHP.trim()) < 1 ||
+            parseInt(car.engine.powerHP.trim()) > 10000
+        ) {
+            errors.enginePowerHP =
+                "The engine power must be between 1 and 10000 horsepower";
+        }
+        if (car.engine.torqueNM.trim() === "") {
+            errors.engineTorqueNM = "Engine Torque (NM) is required";
+        } else if (
+            parseInt(car.engine.torqueNM.trim()) < 1 ||
+            parseInt(car.engine.torqueNM.trim()) > 100000
+        ) {
+            errors.engineTorqueNM =
+                "The engine torque must be between 1 and 100000 Newton meters";
         }
         if (!car.garage) {
             errors.garage = "Garage is required";
@@ -141,9 +192,38 @@ const AddCar = () => {
         )
             .then((response) => {
                 console.log("Car added successfully:", response);
+                toast({
+                    title: "Car Added",
+                    description: "The car has been added successfully.",
+                    status: "success",
+                    duration: 5000,
+                    isClosable: true,
+                });
+                setCar({
+                    name: "",
+                    description: "",
+                    manufacturer: "",
+                    model: "",
+                    year: "",
+                    seats: "",
+                    engine: {
+                        size: "",
+                        fuelType: "",
+                        powerHP: "",
+                        torqueNM: "",
+                    },
+                    garage: "",
+                });
             })
             .catch((error) => {
                 console.error("Error adding car:", error);
+                toast({
+                    title: "Error",
+                    description: "Failed to add the car. Please try again.",
+                    status: "error",
+                    duration: 5000,
+                    isClosable: true,
+                });
             });
     };
 
@@ -216,7 +296,7 @@ const AddCar = () => {
                     />
                     <FormErrorMessage>{formErrors.seats}</FormErrorMessage>
                 </FormControl>
-                <FormControl>
+                <FormControl isInvalid={formErrors.engineSize}>
                     <FormLabel htmlFor="engineSize">Engine Size:</FormLabel>
                     <Input
                         type="text"
@@ -225,8 +305,9 @@ const AddCar = () => {
                         value={car.engine.size}
                         onChange={handleEngineInputChange}
                     />
+                    <FormErrorMessage>{formErrors.engineSize}</FormErrorMessage>
                 </FormControl>
-                <FormControl>
+                <FormControl isInvalid={formErrors.engineFuelType}>
                     <FormLabel htmlFor="engineFuelType">Engine Fuel Type:</FormLabel>
                     <Input
                         type="text"
@@ -235,8 +316,9 @@ const AddCar = () => {
                         value={car.engine.fuelType}
                         onChange={handleEngineInputChange}
                     />
+                    <FormErrorMessage>{formErrors.engineFuelType}</FormErrorMessage>
                 </FormControl>
-                <FormControl>
+                <FormControl isInvalid={formErrors.enginePowerHP}>
                     <FormLabel htmlFor="enginePowerHP">Engine Power (HP):</FormLabel>
                     <Input
                         type="text"
@@ -245,8 +327,9 @@ const AddCar = () => {
                         value={car.engine.powerHP}
                         onChange={handleEngineInputChange}
                     />
+                    <FormErrorMessage>{formErrors.enginePowerHP}</FormErrorMessage>
                 </FormControl>
-                <FormControl>
+                <FormControl isInvalid={formErrors.engineTorqueNM}>
                     <FormLabel htmlFor="engineTorqueNM">Engine Torque (NM):</FormLabel>
                     <Input
                         type="text"
@@ -255,9 +338,18 @@ const AddCar = () => {
                         value={car.engine.torqueNM}
                         onChange={handleEngineInputChange}
                     />
+                    <FormErrorMessage>{formErrors.engineTorqueNM}</FormErrorMessage>
                 </FormControl>
-                <GarageDropdown garages={garages} handleGarageSelect={handleGarageSelect} />
-                <Button type="submit" mt={4} colorScheme="blue">
+                <GarageDropdown
+                    garages={garages}
+                    handleGarageSelect={handleGarageSelect}
+                />
+                <Button
+                    mt={4}
+                    colorScheme="teal"
+                    type="submit"
+                    isDisabled={!garages || garages.length === 0}
+                >
                     Add Car
                 </Button>
             </form>
