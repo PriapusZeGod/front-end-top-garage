86ed1b09763344bafa120362c8d21a0a229c2815
Dragos-Ioan Osoianu <dragos.osoianu@gmail.com>
2023-05-29 15:18:48 +0200

    Revert "test"
    This reverts commit 74b6a0cb07776403b9e1ecde2df37f02baf70d7b.

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,18 @@
 module.exports = {
-  preset: 'react-native',
-  setupFiles: ['./jest.setup.js'],
-  transformIgnorePatterns: [
-    'node_modules/(?!(jest-)?react-native|@react-native-community|@react-navigation)',
-  ],
-  moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/src/$1',
-  },
-  testMatch: ['<rootDir>/src/**/*.test.{js,jsx,ts,tsx}'],
-  testPathIgnorePatterns: ['/node_modules/', '/ios/', '/android/'],
-  collectCoverage: true,
-  coverageDirectory: '<rootDir>/coverage',
-  collectCoverageFrom: ['<rootDir>/src/**/*.{js,jsx,ts,tsx}', '!<rootDir>/src/**/*.test.{js,jsx,ts,tsx}'],
-};
+    transform: {
+      '^.+\\.jsx?$': 'babel-jest',
+    },
+    moduleNameMapper: {
+      "\\.(jpg|jpeg|png|gif|svg)$": "<rootDir>/__mocks__/fileMock.js"
+    },
+
+    transformIgnorePatterns: [
+      "/node_modules/",
+      "^.+\\.css$"
+    ],
+      testEnvironment: "jsdom"
+
+      
+  };
+
+  
